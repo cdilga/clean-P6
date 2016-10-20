34e80ffa64b2932f5dd0bb25ce880fe36da8ea4a
Chris Dilger <cdilg0@eq.edu.au>
2016-10-21 09:04:32 +1000

    initial commit

---
+++ removeStreak.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 15:38:33 2016
+
+@author: Chris Dilger
+"""
+from astropy.io import fits
+import glob
+from scipy.ndimage import median_filter
+
+directory = './fits'
+outdir = './outfits'
+
+target = 579
+lineRemovedString = '-line-removed'
+
+filelist = glob.glob(directory + '/*.fits')
+if len(filelist) == 0:
+    raise ValueError('No fits files in ' + directory)
+
+for filename in filelist:
+    hdulist = fits.open(filename)
+    for i in range(len(hdulist[0].data[579])):
+        hdulist[0].data[i][target] = (hdulist[0].data[i][target+1] + hdulist[0].data[i][target-1]) / 2
+        hdulist[0].data = median_filter(hdulist[0].data, size=3)
+    hdulist.writeto(outdir + filename + lineRemovedString + '.fits')
+#print hdulist[0].data[579][0]
+    
+#hdulist = fits.open('fits/Initial Cyg X-1 P6_1325181_B_001.fits')
