471e45b6de20c0f3fa742a31749f09f0d93e607a
Chris Dilger <cdilg0@eq.edu.au>
2016-10-21 09:27:34 +1000

    sequentially process with progress

--- removeStreak.py
+++ removeStreak.py
@@ -7,23 +7,32 @@ Created on Thu Oct 20 15:38:33 2016
 from astropy.io import fits
 import glob
 from scipy.ndimage import median_filter
+import os
 
-directory = './fits'
-outdir = './outfits'
+directory = '.\fits'
+outdir = '.\outfits'
 
 target = 579
 lineRemovedString = '-line-removed'
 
-filelist = glob.glob(directory + '/*.fits')
+filelist = glob.glob(directory + '\*.fits')
 if len(filelist) == 0:
     raise ValueError('No fits files in ' + directory)
 
 for filename in filelist:
     hdulist = fits.open(filename)
+    print 'processing ' + filename
     for i in range(len(hdulist[0].data[579])):
         hdulist[0].data[i][target] = (hdulist[0].data[i][target+1] + hdulist[0].data[i][target-1]) / 2
         hdulist[0].data = median_filter(hdulist[0].data, size=3)
-    hdulist.writeto(outdir + filename + lineRemovedString + '.fits')
+    filename = filename.replace(directory, '')
+    outfilename = outdir + filename + lineRemovedString + '.fits'
+    if os.path.exists(outdir):
+        hdulist.writeto(outfilename)
+    else:
+        os.mkdir(outdir)
+        hdulist.writeto(outfilename)
+
 #print hdulist[0].data[579][0]
     
 #hdulist = fits.open('fits/Initial Cyg X-1 P6_1325181_B_001.fits')
